4dfc1cbe9a5e729028d5f415b21d1c38167a56f2
Ephriam Henderson <hendersonephriamn@gmail.com>
2020-03-25 09:58:44 -0500

    Added email validation to player model.

--- db/models/player/index.js
+++ db/models/player/index.js
@@ -1,30 +1,34 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
+// From https://emailregex.com/
+const emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-const Player= new Schema({
+const Player = new Schema({
 	username: {
 		type: String,
 		required: true,
-		unique: true,
+		unique: true
 	},
 	email: {
 		type: String,
 		required: true,
-		index: { unique: true} 
+		index: { unique: true },
+		lowercase: true,
+		match: emailPattern
 	},
 	password: {
 		type: String,
-		required: true,
+		required: true
 	}
 });
 
 Player.methods.checkPassword = function(plainTextPassword) {
-	return bcrypt.compare(plainTextPassword, this.password)
-}
+	return bcrypt.compare(plainTextPassword, this.password);
+};
 
 Player.statics.hashPassword = function(plainTextPassword) {
-	return bcrypt.hash(plainTextPassword, 10)
-}
+	return bcrypt.hash(plainTextPassword, 10);
+};
 
-module.exports = Player
+module.exports = Player;
